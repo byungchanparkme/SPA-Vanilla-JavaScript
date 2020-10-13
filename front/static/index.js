8c8ada2c97ddb9e1434356995aeaf969aa4900e2
Byungchan-Park <pbc9236@gmail.com>
2020-10-13 13:19:25 +0900

    feat: Write the actual routes on the client side
    <description>
    While the server is working, only files on the absolute path("front/static") is provided.
    To check whether current address is right or not,
    use location object's property "path" to compare between current address and route's path.

---
+++ front/static/index.js
@@ -0,0 +1,31 @@
+// write the actual client side router
+const router = async () => {
+  const routes = [
+    { path: "/", view: () => console.log("Viewing Dashboard") },
+    { path: "/posts", view: () => console.log("Viewing Posts") },
+    { path: "/settings", view: () => console.log("Viewing Settings") },
+  ]
+
+  // Test each route for potential match
+  const potentialMatches = routes.map((route) => {
+    return {
+      route: route,
+      isMatch: location.pathname === route.path,
+    }
+  })
+
+  let match = potentialMatches.find((potentialMatch) => potentialMatch.isMatch)
+
+  // default match route
+  if (!match) {
+    match = {
+      route: routes[0],
+      isMatch: true,
+    }
+  }
+  console.log(match.route.view())
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  router()
+})
--- server.js
+++ server.js
@@ -3,9 +3,10 @@ const path = require("path")
 
 const app = express()
 
-// every route or every path goes to index.html
+app.use("/static", express.static(path.resolve(__dirname, "front", "static")))
+
 app.get("/*", (req, res) => {
-  res.sendFile(path.resolve("front", "index.html"))
+  res.sendFile(path.resolve(__dirname, "front", "index.html"))
 })
 
 app.listen(process.env.PORT || 5060, () => console.log("Server is running..."))
